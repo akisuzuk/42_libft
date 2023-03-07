46afca4e987562366b80746921bc7a9216ebd432
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-03-07 11:38:19 +0900

    20230307

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akisuzuk <XXX>                             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 18:40:53 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/03/02 18:16:42 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/03/07 11:31:29 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -23,10 +21,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || !set)
 		return (NULL);
-	if (!set || s1[0] == '\0')
-		return (ft_calloc(1, 1));
+	if (s1[0] == '\0')
+		return ((char *)ft_calloc(1, 1));
 	head = 0;
-	while (ft_strchr(set, s1[head]))
+	while (ft_strchr(set, s1[head]) && s1[head + 1] != '\0')
 		head++;
 	tail = ft_strlen((char *)s1) - 1;
 	while (ft_strchr(set, s1[tail]) && head < tail)
@@ -34,7 +32,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (head <= tail)
 		ret = malloc(sizeof(char) * (tail - head + 1));
 	if (tail < head || !ret)
-		return (ft_calloc(1, 1));
+		return ((char *)ft_calloc(1, 1));
 	i = 0;
 	while (head <= tail)
 		ret[i++] = s1[head++];
@@ -42,15 +40,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (ret);
 }
 
-// int	main(void)
-// {
-// 	char	str1[] = "   xxx   xxx";
-// 	char	str2[] = " x";
-// 	char	*joined;
-// 	printf("str1=%s\n", str1);
-// 	printf("str2=%s\n", str2);
-// 	joined = ft_strtrim(str1, str2);
-// 	printf("ret=%s\n", joined);
-// 	//printf("strcmp=%d\n", strcmp(joined, "42"));
-// 	free(joined);
-// }
+//int	main(void)
+//{
+//	//char	str1[] = "   xxx   xxx";
+//	//char	str2[] = " x";
+//	//char	str1[] = "lorem ipsum dolor sit amet";
+//	//char	str2[] = "te";
+//	char	*joined;
+//	printf("str1=%s\n", str1);
+//	printf("str2=%s\n", str2);
+//	printf("strlen(str2)=%lu\n", strlen(str2));
+//	printf("---------------\n");
+//	joined = ft_strtrim(str1, str2);
+//	printf("---------------\n");
+//	printf("ret=%s\n", joined);
+//	//printf("strcmp=%d\n", strcmp(joined, "42"));
+//	free(joined);
+//}
