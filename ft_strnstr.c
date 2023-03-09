c39209b1b471631f08d3afda89c0beb08765a625
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-03-09 16:41:04 +0900

    20230309

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: akisuzuk <XXX>                             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 16:40:49 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/03/08 23:51:53 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/03/09 16:22:46 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,41 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
-char	*search(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int		i;
-	char	*ret;
+	size_t	i;
+	size_t	j;
 
+	if (needle[0] == '\0')
+		return ((char *)(haystack));
 	i = 0;
-	while (*haystack && len)
+	while (i < len && haystack[i])
 	{
-		if (*haystack == needle[0])
-		{
-			ret = (char *)haystack;
-			i = 0;
-			while (*haystack == needle[i] && len--)
-			{
-				i++;
-				haystack++;
-				if (!needle[i])
-					return (ret);
-			}
-			haystack -= i;
-			len += i;
-		}
-		haystack++;
-		len--;
+		j = 0;
+		while (haystack[i + j] && needle[j] && i + j < len && haystack[i
+				+ j] == needle[j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)(haystack + i));
+		i++;
 	}
 	return (NULL);
 }
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
-	char	*ret;
-
-	if (haystack == NULL)
-		return (NULL);
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	ret = search(haystack, needle, len);
-	if (ret)
-		return (ret);
-	else
-		return (NULL);
-}
-
-//
 //int	main(void)
 //{
 //	char	str1[] = "aaabcabcd";
 //	char	str2[] = "aabc";
-//
-//	printf("%s\n", str1);
-//	printf("%s\n", str2);
-//	printf("origin ret = %s\n", strnstr(str1, "abcd", 9));
-//	printf("myfunc ret = %s\n", ft_strnstr(str1, "abcd", 9));
-//	printf("---case114---\n");
-//	printf("origin ret = %s\n", strnstr("", "", 0));
-//	printf("myfunc ret = %s\n", ft_strnstr("", "", 0));
-//	printf("---case115---\n");
-//	printf("origin ret = %s\n", strnstr(NULL, "1", 0));
-//	printf("myfunc ret = %s\n", ft_strnstr(NULL, "1", 0));
+
+//	//printf("%s\n", str1);
+//	//printf("%s\n", str2);
+//	//printf("origin ret = %s\n", strnstr(str1, "abcd", 9));
+//	//printf("myfunc ret = %s\n", ft_strnstr(str1, "abcd", 9));
+//	//printf("---case114---\n");
+//	//printf("origin ret = %s\n", strnstr("", "", 0));
+//	//printf("myfunc ret = %s\n", ft_strnstr("", "", 0));
+//	//printf("---case115---\n");
+//	printf("origin ret = %s\n", ft_strnstr("fake", NULL, 0));
+//	printf("myfunc ret = %s\n", strnstr("fake", NULL, 0));
 //	return (0);
 //}
-//
